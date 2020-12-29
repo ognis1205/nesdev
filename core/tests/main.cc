0220ac379072c8e636fd8471c376c22af59d64a4
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2020-12-29 13:23:01 +0900

    implement googletest unittests.

---
+++ core/tests/main.cc
@@ -0,0 +1,13 @@
+/*
+ * nes-emulator:
+ * Emulator for the Nintendo Entertainment System (R) Archetecture.
+ * Written by and Copyright (C) 2020 Shingo OKAWA shingo.okawa.g.h.c@gmail.com
+ * Trademarks are owned by their respect owners.
+ */
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleMock(&argc, argv);
+  return RUN_ALL_TESTS();
+}
---
+++ core/tests/test_mmu.cc
@@ -0,0 +1,32 @@
+/*
+ * nes-emulator:
+ * Emulator for the Nintendo Entertainment System (R) Archetecture.
+ * Written by and Copyright (C) 2020 Shingo OKAWA shingo.okawa.g.h.c@gmail.com
+ * Trademarks are owned by their respect owners.
+ */
+#define BOOST_TEST_MAIN
+#include <memory>
+#include <gtest/gtest.h>
+#include "nes/core/exceptions.h"
+#include "nes/core/types.h"
+#include "detail/mmu.h"
+
+class MMUTest : public testing::Test {
+ protected:
+  void SetUp() override {
+    start_time_ = time(nullptr);
+  }
+
+  void TearDown() override {
+    const time_t end_time = time(nullptr);
+    EXPECT_TRUE(end_time - start_time_ <= 5) << "The test took too long";
+  }
+
+  time_t start_time_;
+
+  nes::core::detail::MMU mmu;
+};
+
+TEST_F(MMUTest, Clear) {
+//  mmu.Add();
+}
