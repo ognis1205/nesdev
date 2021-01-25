f64e5c4637c8c1c95d59282dc4462c47dd9cb389
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2021-01-25 21:14:30 +0900

    implement CPU/PPU factory.

---
+++ core/src/ppu_factory.cc
@@ -0,0 +1,29 @@
+/*
+ * nes-emulator:
+ * Emulator for the Nintendo Entertainment System (R) Archetecture.
+ * Written by and Copyright (C) 2020 Shingo OKAWA shingo.okawa.g.h.c@gmail.com
+ * Trademarks are owned by their respect owners.
+ */
+#include <memory>
+#include "nesdev/core/ppu.h"
+#include "nesdev/core/ppu_factory.h"
+#include "nesdev/core/mmu.h"
+#include "nesdev/core/palettes.h"
+#include "detail/rp2c02.h"
+
+namespace nesdev {
+namespace core {
+
+std::unique_ptr<PPU> PPUFactory::RP2C02(PPU::Registers* const registers,
+					PPU::Shifters* const shifters,
+					MMU* const mmu) {
+  return std::make_unique<detail::RP2C02>(
+    std::make_unique<PPU::Chips>(std::make_unique<PPU::ObjectAttributeMap<64>>()),
+    registers,
+    shifters,
+    mmu,
+    Palettes::RP2C02());
+}
+
+}  // namespace core
+}  // namespace nesdev
