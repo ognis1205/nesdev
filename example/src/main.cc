4d8a342b3d497e1f223d38194f0835b427422130
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2021-01-26 16:32:23 +0900

    create example project.

---
+++ example/src/main.cc
@@ -0,0 +1,14 @@
+
+/*
+ * nes-emulator:
+ * Emulator for the Nintendo Entertainment System (R) Archetecture.
+ * Written by and Copyright (C) 2020 Shingo OKAWA shingo.okawa.g.h.c@gmail.com
+ * Trademarks are owned by their respect owners.
+ */
+#include <iostream>
+#include <memory>
+#include "nesdev/core.h"
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv) {
+  std::cout << "Hello, Nesdev!" << std::endl;
+}
